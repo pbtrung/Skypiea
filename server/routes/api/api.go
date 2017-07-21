be8fee3ff1f38aec7fd7bc47f755592610273eea
Trung Pham <phambaotrung@gmail.com>
2017-07-21 11:45:08 -0700

    Try to fix

--- server/routes/api/api.go
+++ server/routes/api/api.go
@@ -5,13 +5,12 @@ import (
 	"net/http"
 	"os"
 
-	_ "github.com/mattn/go-sqlite3"
 	"github.com/pbtrung/Skypiea/server/app"
 )
 
-var db = app.InitDB(os.Getenv("DB_PATH"))
-
 func GetBookById(w http.ResponseWriter, r *http.Request) {
+	var db = app.InitDB(os.Getenv("DB_PATH"))
+
 	rows, err := db.Query("SELECT * FROM books WHERE id < 10")
 	checkErr(err)
 	fmt.Println(rows)

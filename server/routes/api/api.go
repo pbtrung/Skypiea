05c87b6547b29267343701a832a279b50d5335ea
Trung Pham <phambaotrung@gmail.com>
2017-07-21 09:31:41 -0700

    Fix name

--- server/routes/api/api.go
+++ server/routes/api/api.go
@@ -4,6 +4,6 @@ import (
 	"net/http"
 )
 
-func APIMiddleware(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+func GetBookById(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	w.Write([]byte("Get a book!"))
 }

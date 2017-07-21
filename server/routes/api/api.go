facb589df8bf116b07b983848be17688fce29914
Trung Pham <phambaotrung@gmail.com>
2017-07-21 12:04:40 -0700

    Try rows

--- server/routes/api/api.go
+++ server/routes/api/api.go
@@ -1,19 +1,19 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
 	"github.com/pbtrung/Skypiea/server/app"
 )
 
 var db = app.InitDB(os.Getenv("DB_PATH"))
 
 func GetBookById(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-	w.Write([]byte("Get a book " + id))
+	rows, err := db.Query("SELECT * FROM books WHERE id < 10")
+	checkErr(err)
+	fmt.Println(rows)
 }
 
 func checkErr(err error) {

353fa309b1dcae29b33746361cd6a5a9e9ff0e18
Trung Pham <phambaotrung@gmail.com>
2017-07-21 00:10:43 -0700

    Complete server

--- server/server.go
+++ server/server.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func StartServer() {
 	r := mux.NewRouter()
+	r.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("public/"))))
+	http.Handle("/", r)
+	http.ListenAndServe(":3000", nil)
 }
